2b3179cc6e637dbbe111b62ec751d9a99edf0549
Wanguang <wanguang@example.com>
2026-07-12 15:54:33 +0800

    Update launcher icon for V1.1.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "io.github.splashcurry.pinyinpractice"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
